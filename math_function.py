a791e3b6a0965c01548cf539e87b2e318c71d4bc
roey_averbuch <roey6922@gmail.com>
2025-06-19 15:40:06 +0300

    adding is praim func

--- math_function.py
+++ math_function.py
@@ -3,22 +3,19 @@
 """
 def welcome():
     print("wolcome to my algorithems functions")
-
-user_input=int(input())
-
+def isprime(num,i=2):
+    if num == i:
+        return True
+    elif num%i==0:
+        return False
+    return isprime(num,i+1)
 def factorial(num):
     """calculate num factorial"""
     if num ==1:
         return num
     return num * factorial(num-1)
 
-
 if __name__=="__main__":
     welcome()
-    print(factorial(user_input))
-
-
-
-
-
-
+    isprime(5)
+    factorial(7)
