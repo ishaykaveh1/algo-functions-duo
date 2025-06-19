67c84f767cb9532e1eb3de4eb144c430fe48aeaa
roey_averbuch <roey6922@gmail.com>
2025-06-19 14:42:02 +0300

    adding factorial func

--- math_function.py
+++ math_function.py
@@ -5,4 +5,12 @@ def welcome():
     print("wolcome to my algorithems functions")
 if __name__=="__main__":
     welcome()
-    
+
+user_input=int(input())
+
+def factorial(num):
+    """calculate num factorial"""
+    if num ==1:
+        return num
+    return num * factorial(num-1)
+print(factorial(user_input))
